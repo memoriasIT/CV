0f1ea4a64e302bb3d477bb9b8d0e2332d5fd1744
memoriasIT <memoriasdeuninformatico2017@gmail.com>
2021-09-20 13:56:17 +0200

    update resume

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      textColor: {
+        skin: {
+          base: "var(--color-text-base)",
+        }
+      }
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
